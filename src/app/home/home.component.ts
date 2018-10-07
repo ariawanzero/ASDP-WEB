6f234999287cbb5c4f99cf087d055c071717dd14
ariawanzero <ariawan.zero@gmail.com>
2018-10-08 06:33:13 +0700

    add list user

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SidebarService } from '../shared/service/sidebar.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  isOpen: boolean = false;
 
-  constructor() { }
+  constructor(
+    private sidebarService: SidebarService
+  ) { }
 
   ngOnInit() {
+    this.changeToogleSidebar();
   }
 
+  private changeToogleSidebar(): void {
+    this.sidebarService.listenStatus().subscribe(
+      state => {
+        this.isOpen = state;
+      }, err => {
+        this.isOpen = false;
+      }
+    );
+  }
 }
