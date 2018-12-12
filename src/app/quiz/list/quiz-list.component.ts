2081451fb29a14f9bf13022022f2dc5d17ce2b07
ariawanzero <ariawan.zero@gmail.com>
2018-12-12 10:41:42 +0700

    add lst quiz

--- src/app/quiz/list/quiz-list.component.ts
+++ src/app/quiz/list/quiz-list.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { BlockUI, NgBlockUI } from 'ng-block-ui';
+
+import { GlobalMessageService } from '../../shared/service/global-message.service';
+
 
 @Component({
-  selector: 'app-quiz-list',
+  selector: 'asdp-quiz-list',
   templateUrl: './quiz-list.component.html',
   styleUrls: ['./quiz-list.component.css']
 })
