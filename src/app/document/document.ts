a6a2fae026bb4210eaeeba18c477e2ece6836421
ariawanzero <ariawan.zero@gmail.com>
2019-01-17 19:41:55 +0700

    add document input

---
+++ src/app/document/document.ts
@@ -0,0 +1,14 @@
+export class Document {
+    id: string;
+    name: string;
+    description: string;
+    type: string;
+    sop: string;
+    category: string;
+    divisi: string;
+    tumbnail: string;
+    startDate: string;
+    endDate: string;
+    nameFileJson: string;
+    status: string;
+}
---
+++ src/app/document/input/document-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-document-input',
+  templateUrl: './document-input.component.html',
+  styleUrls: ['./document-input.component.css']
+})
+export class DocumentInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
